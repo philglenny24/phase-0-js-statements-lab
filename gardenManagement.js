0c680569680d4c4abf2a9d6e0280ac848910f51d
philglenny24 <glennytronephilip@gmail.com>
2026-03-25 00:28:53 +0300

    edited the gardenmanagent.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,20 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+ if (temperature > 85) {
+    console.log("Watering on")
+ }else {
+    console.log("Watering off")
+ }
+
+ if (timeOfDay === "evening") {
+    console.log("Lights on")
+ }else {
+    console.log("Lights off")
+ }
+
+ while (soilMoisture < 40) {
+  soilMoisture += 5;
+  console.log(soilMoisture);
+}
